739fd0814ff35b021e5555fee2a1ea6713b13774
Nithiwat Kampanya <mappies@gmail.com>
2020-04-03 19:24:57 -0400

    Expose errors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './dbColumn';
 export * from './dbTable';
 export * from './nodeNamo';
+
+export * from './errors/nodenamoError';
+export * from './errors/validationError';
+export * from './errors/versionError';
